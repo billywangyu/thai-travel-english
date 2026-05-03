356e8e4f0bcb286017ff155f150dd3b29a6b206d
bi2024 <billy3online@hotmail.com>
2026-05-04 04:27:11 +0800

    final full fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from '@vitejs/plugin-vue'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/thai-travel-english/'
+  base: "/thai-travel-english/"
 })
