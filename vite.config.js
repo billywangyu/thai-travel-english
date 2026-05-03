75a0f851a110a286795c05f6735997496bc79b04
bi2024 <billy3online@hotmail.com>
2026-05-04 03:54:01 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins: [vue()], base: '/thai-travel-english/' })
+export default defineConfig({ plugins: [vue()], base: '' })
